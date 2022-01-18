b87c90cff663b3e257f3ce7a0b8c6dbfae906704
yakattak <stevecalebrese@gmail.com>
2022-01-17 22:13:29 -0500

    comment on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
+
+// get current day, current time, current hour
 var currentHour = new Date().getHours();
 
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
 
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
 
+//when save button is clicked save item
 $(".saveBtn").on("click", function() {
+    //find parent div of saveBtn then find the text area inside
     var comment = $(this).closest("div").children(".textarea");
     var saveId = $(comment).attr("id");
     var text = $(comment).text().trim();
-    console.log("saving " + saveId)
+    //console.log("saving " + saveId)
+
+    //save by ID of text area
     localStorage.setItem(saveId, JSON.stringify(text));
 
 })
 
-
+//when text area is no longer the focus change the text value to new input
 $(".schedule").on("blur", "textarea", function() {
     // get current 2 of textarea
     var text = $(this).val();
@@ -34,31 +44,21 @@ var loadTasks = function() {
     //add class for color coding
     if (parseInt(dataTime) <parseInt(currentHour)) {
         $(this).addClass("past");
-    }
+    };
 
     if (parseInt(dataTime) === parseInt(currentHour)) {
         $(this).addClass("present");
-    }
+    };
 
     if (parseInt(dataTime) > parseInt(currentHour)) {
         $(this).addClass("future");
-    }
+    };
     
-    })
+    });
     
-
-
-
-    
+   
 
 }
-
-//check if after current time
-
-
-
-
-
 
 loadTasks();
 
